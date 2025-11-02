89e14c7b080165b8f72157816d130933fe5c9670
Shadman <sshihab10@gmail.com>
2025-11-02 22:07:44 +0600

    App 'Sign Up' and 'Login' pages

--- app-frontend/app/src/main/java/too/good/crm/MainActivity.kt
+++ app-frontend/app/src/main/java/too/good/crm/MainActivity.kt
@@ -4,13 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import too.good.crm.features.login.LoginScreen
+import too.good.crm.features.signup.SignupScreen
 import too.good.crm.ui.theme.TooGoodCrmTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +32,23 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TooGoodCrmTheme {
+                val navController = rememberNavController()
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    NavHost(
+                        navController = navController,
+                        startDestination = "main",
                         modifier = Modifier.padding(innerPadding)
-                    )
+                    ) {
+                        composable("main") {
+                            MainScreen(navController = navController)
+                        }
+                        composable("login") {
+                            LoginScreen()
+                        }
+                        composable("signup") {
+                            SignupScreen()
+                        }
+                    }
                 }
             }
         }
@@ -31,17 +56,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen(navController: NavController) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { navController.navigate("login") }) {
+            Text("Login")
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { navController.navigate("signup") }) {
+            Text("Sign Up")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MainScreenPreview() {
     TooGoodCrmTheme {
-        Greeting("Android")
+        val navController = rememberNavController()
+        MainScreen(navController)
     }
 }
